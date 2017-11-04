94f8eb0c8293e8c9e0c7c1afdd7edaa27a0e4043
Freddie Ofori <FrederickOfori@email.com>
2017-11-04 12:44:48 +0000

    JSON request added to route - openweathermap API

--- app.rb
+++ app.rb
@@ -1,13 +1,27 @@
 require 'sinatra'
+require 'json'
+require 'open-uri'
 
 class API < Sinatra::Base
+  @@weather_city = {}
 
   get '/' do
-    # 'Hello from planet earth'
+    @@weather_city.each do |city_name, weather_string|
+      data = open("http://api.openweathermap.org/data/2.5/weather?q=#{city_name}&APPID=a329ae0dc2be2fdc551afc41de40704f")
+      parsed_data = JSON.parse(data.read)
+      @@weather_city[city_name] = parsed_data["weather"].first["main"]
+    end
+    @list_of_cities_and_weather = @@weather_city
     erb(:index)
   end
 
-  run! if app_file == $0
+  post '/' do
+    @city  = params[:city]
+    @@weather_city[@city] = nil
+    redirect "/"
+  end
+
+  # run! if app_file == $0
 
 end
 
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,6 +1,29 @@
-feature 'Testing infrastructure' do
-    scenario 'Can run app and check page content' do
+feature 'Testing homepage' do
+    xit 'Can run app and check page content' do
       visit('/')
-      expect(page).to have_content('Hello from planet earth')
+      fill_in('city', :with => 'London')
+      click_button('Submit')
+      expect(page).to have_content('Hello from London')
+    end
+
+    xit 'Can output multiple cities' do
+      visit('/')
+      fill_in('city', :with => 'London')
+      click_button('Submit')
+      fill_in('city', :with => 'Barcelona')
+      click_button('Submit')
+      expect(page).to have_content("Hello from London\nHello from Barcelona")
+    end
+
+    scenario 'Can output multiple cities' do
+      visit('/')
+      fill_in('city', :with => 'London')
+      click_button('Submit')
+      fill_in('city', :with => 'Barcelona')
+      click_button('Submit')
+      expect(page).to have_content("Hello from London - Rain \nHello from Barcelona - Clouds")
     end
   end
+
+
+
