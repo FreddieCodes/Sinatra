61017f7e4f2fbce1d440eaa615dbace5a28f48ca
Freddie Ofori <FrederickOfori@email.com>
2017-11-06 15:21:38 +0000

    seperated production gems from testing gems

--- Gemfile
+++ Gemfile
@@ -3,11 +3,14 @@
 source 'https://rubygems.org'
 
 gem 'rake'
-gem 'rspec'
-gem 'rubocop'
-gem 'simplecov', require: false, group: :test
-gem 'simplecov-console', require: false, group: :test
 gem 'sinatra'
-gem 'capybara'
-gem 'selenium-webdriver'
-gem "chromedriver-helper"
+
+group :test do
+    gem 'rspec'
+    gem 'rubocop'
+    gem 'simplecov', require: false, group: :test
+    gem 'simplecov-console', require: false, group: :test
+    gem 'capybara'
+    gem 'selenium-webdriver'
+    gem "chromedriver-helper"
+end
--- app.rb
+++ app.rb
@@ -1,13 +1,13 @@
 require 'sinatra'
 require 'json'
 require 'open-uri'
-
+# API KEY = a329ae0dc2be2fdc551afc41de40704f
 class API < Sinatra::Base
   @@weather_city = {}
 
   get '/' do
     @@weather_city.each do |city_name, weather_string|
-      data = open("http://api.openweathermap.org/data/2.5/weather?q=#{city_name}&APPID=a329ae0dc2be2fdc551afc41de40704f")
+      data = open("http://api.openweathermap.org/data/2.5/weather?q=#{city_name}&APPID=a329ae0dc2be2fdc551afc41de40704f") # API Key at the end
       parsed_data = JSON.parse(data.read)
       @@weather_city[city_name] = parsed_data["weather"].first["main"]
     end
